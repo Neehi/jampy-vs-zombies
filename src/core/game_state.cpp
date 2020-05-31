7dd41d5aae4220a54c970989e2abc70c4d733d42
Neehi <nick.snape@hotmail.com>
2020-05-31 19:43:13 +0100

    Update GameState

--- src/core/game_state.cpp
+++ src/core/game_state.cpp
@@ -2,6 +2,18 @@
 
 #include <iostream>
 
+#include "core/game.h"
+#include "core/game_state_manager.h"
+#include "input/input_manager.h"
+
+const Game& GameState::GetGame() const {
+  return state_manager_->GetGame();
+}
+
+const InputManager& GameState::GetInputManager() const {
+  return GetGame().GetInputManager();
+}
+
 void GameState::OnEnter() {
   std::cout << "GameState: Entering '" << GetID() << "' state\n";
 }
--- src/core/game_state.h
+++ src/core/game_state.h
@@ -8,7 +8,9 @@
 
 #include "graphics/renderer.h"
 
+class Game;
 class GameStateManager;
+class InputManager;
 
 // GameState represents a stage of a game, i.e. title screen, main menu, pause,
 // gameplay, etc. Each state is intended to be isolated from the game engine
@@ -17,20 +19,35 @@ class GameState {
  public:
   GameState() = default;
 
-  inline GameStateManager& GetStateManager() const {
+  // Return the string identifier for this state
+  virtual std::string GetID() const = 0;  // XXX: Pass in?
+
+  // Returns a reference to the game
+  const Game& GetGame() const;
+
+  // Return a reference to the game's input manager
+  const InputManager& GetInputManager() const;
+
+  // Returns a reference to the state manager, which manages state changes
+  inline const GameStateManager& GetStateManager() const {
     assert(state_manager_);
     return *state_manager_;
   }
 
+  // Called when this state becomes the active state
   virtual void OnEnter();
+
+  // Called then this state is exited - i.e. moving to another state
   virtual void OnExit();
 
+  // Called each game loop - when the active state
   virtual void OnUpdate(const float delta) = 0;
+
+  // Called each game loop - when the active state
   virtual void OnRender(const Renderer& renderer) = 0;
 
-  virtual std::string GetID() const = 0;
-
  private:
+  // Should not be called directly (called by GameStateManager)
   inline void SetStateManager(
       GameStateManager& state_manager) {  // NOLINT(runtime/references)
     state_manager_ = &state_manager;
